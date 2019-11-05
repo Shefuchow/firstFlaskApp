a8d5805b88c26eda10222ad9eae73a98fbd79a2d
Mohammed Sefath Chowdhury <mschowdh@buffalo.edu>
2019-11-04 19:56:16 -0500

    practiced making different routes and functions calls

--- weatherApp/helloWorld.py
+++ weatherApp/helloWorld.py
@@ -4,6 +4,13 @@ helloWorld = Flask(__name__)      # create an app instance
 
 @helloWorld.route("/")            # at the end point /
 def hello():                      # call method hello
-    return "Hello World!"         # which returns "hello world"
+    return "Hello World! "        # which returns "hello world"
+
+
+@helloWorld.route("/<name>")      # at the end point /<name>
+def hello_name(name):             # call method hello_name
+    return "Hello "+ name + "!"   # which returns "hello + name + !"
+
+
 if __name__ == "__main__":        # on running python app.py
-    helloWorld.run()              # run the flask app
+    helloWorld.run(debug=True)    # run the flask app
